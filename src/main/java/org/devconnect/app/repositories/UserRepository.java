61bc7c73b12b0354230ee2d6cf16a53210cfc5b1
sanan <sanan.tahmazli@gmail.com>
2025-12-07 15:04:53 +0400

    Create UserRepository

---
+++ src/main/java/org/devconnect/app/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package org.devconnect.app.repositories;
+
+import org.devconnect.app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
