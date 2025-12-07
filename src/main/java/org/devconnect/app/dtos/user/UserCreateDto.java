103f08dfce1efe250dd6dd39ff653cedbed3190f
sanan <sanan.tahmazli@gmail.com>
2025-12-07 15:31:43 +0400

    Renamed DTO

--- src/main/java/org/devconnect/app/dtos/user/UserCreateDto.java
+++ src/main/java/org/devconnect/app/dtos/user/UserCreateDto.java
@@ -3,7 +3,7 @@ package org.devconnect.app.dtos.user;
 import lombok.Data;
 
 @Data
-public class UserCreationDto {
+public class UserCreateDto {
     private String name;
     private String surname;
 }
