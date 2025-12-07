23c4d57289c79bb6d94a3f1e5623daa085d057cd
sanan <sanan.tahmazli@gmail.com>
2025-12-07 14:11:58 +0400

    Created project

---
+++ src/main/java/org/devconnect/app/TestAppApplication.java
@@ -0,0 +1,11 @@
+package org.devconnect.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication {
+    static void main(String[] args) {
+        SpringApplication.run(TestAppApplication.class, args);
+    }
+}
